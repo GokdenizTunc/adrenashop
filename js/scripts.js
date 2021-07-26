6c520eba07e3243a2e6f5f8187fd42b1cf9e0a81
InvaderGTK <66417772+InvaderGTK@users.noreply.github.com>
2021-07-26 21:52:56 +0300

    3

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyBacp9jBM_Dk216YtgwrU8siZTOOnHZ3is",
+    authDomain: "adrena-2ed25.firebaseapp.com",
+    projectId: "adrena-2ed25",
+    storageBucket: "adrena-2ed25.appspot.com",
+    messagingSenderId: "278223223450",
+    appId: "1:278223223450:web:7f563e57b4d503d4a9bd23",
+    measurementId: "G-15L7LJS467"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
